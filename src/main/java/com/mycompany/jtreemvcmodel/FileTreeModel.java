340a81b3cd803047df482daad4e906b868cd1395
Daniil <danilp2505@gmail.com>
2018-08-11 15:33:49 +0300

    Working version, refactored

--- src/main/java/com/mycompany/jtreemvcmodel/FileTreeModel.java
+++ src/main/java/com/mycompany/jtreemvcmodel/FileTreeModel.java
@@ -9,7 +9,6 @@ import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
-import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -55,25 +54,13 @@ public class FileTreeModel implements TreeModel {
     public void valueForPathChanged(TreePath path, Object newValue) {
     }
 
-    void fireAddFile(Facade file, TreePath path) {
-        int[] childIndices = getNewChildIndices(file);
+    public void fireAddFile(Facade file, TreePath path) {
+        int[] childIndices = new int[]{getIndexOfChild(file.getParent(), file)};
         Facade[] children = new Facade[]{file};
         TreeModelEvent evt = new TreeModelEvent(this, path, childIndices, children);
         for (TreeModelListener l : listeners) {
             l.treeNodesInserted(evt);
         }
-    }
-
-    private int[] getNewChildIndices (Facade file) {
-        File child = file.getFile();
-        String fileName = child.getName();
-        String[] listParentFileNames = child.getParentFile().list();
-        for (int i = 0; i < listParentFileNames.length; i++) {
-            if (listParentFileNames[i].equals(fileName)) {
-                return new int[]{i};
-            }
-        }
-        return null;
     }
 
     @Override
